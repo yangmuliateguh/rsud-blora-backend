7fd17be103badbe9c49026f69d9a9115460c30f5
yangmuliateguh <teguh.kahfiyasin@gmail.com>
2025-09-15 00:02:21 +0700

    setup test.service.js

--- src/modules/test/test.service.js
+++ src/modules/test/test.service.js
@@ -1,13 +1,13 @@
 const supabase = require('../../config/supabase');
 
 async function getAll() {
-  const { data, error } = await supabase.from('Test').select('*');
+  const { data, error } = await supabase.from('test').select('*');
   if (error) throw error;
   return data;
 }
 
 async function create(name) {
-  const { data, error } = await supabase.from('Test').insert([{ name }]);
+  const { data, error } = await supabase.from('test').insert([{ name }]);
   if (error) throw error;
   return data;
 }
