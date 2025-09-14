7b04b87965eb603553765150b79c78e7e8e17773
yangmuliateguh <teguh.kahfiyasin@gmail.com>
2025-09-15 00:11:13 +0700

    setup routes/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,5 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api', routes);
-app.get('/', (req, res) => {
-  res.send('RSUD Blora Backend API is running');
-});
+
 module.exports = app; 
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,7 @@ const testController = require('../modules/test/test.controller');
 
 router.get('/test', testController.getTest);
 router.post('/test', testController.addTest);
-
+router.get('/', (req, res) => {
+  res.send('RSUD Blora API is ready');
+});
 module.exports = router;
